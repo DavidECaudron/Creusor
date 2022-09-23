f1dc798e312f52e5487552c68642dc4236e48561
DavidECaudron <david-e.caudron@outlook.fr>
2022-09-23 09:14:17 +0200

    Chest Destroy Fix

---
+++ Assets/Ca Me Pele Les Couilles Scripts/Scripts/Coconut.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace caca
+{
+    public class Coconut : MonoBehaviour
+    {
+        #region Util
+
+        private void OnDestroy()
+        {
+            
+        }
+
+        #endregion
+    }
+}
